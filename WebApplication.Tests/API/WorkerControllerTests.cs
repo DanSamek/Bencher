cfb032f5ae8980274a595abef2d33161608a4038
Daniel Samek <samekda3@fit.cvut.cz>
2025-09-28 22:13:03 +0200

    Created solution for webapplication tests

--- WebApplication.Tests/API/WorkerControllerTests.cs
+++ WebApplication.Tests/API/WorkerControllerTests.cs
@@ -1,6 +1,43 @@
+using Microsoft.AspNetCore.Identity;
+using WebApplication.API;
+using WebApplication.Data.Models;
+using WebApplication.Stores;
+
 namespace WebApplication.Tests.API;
 
 public class WorkerControllerTests
 {
+    private WorkerController _controller;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var factory = new TestContextFactory();
+        // Prepare controller.
+        _controller = new WorkerController(new UserStore(factory), new WorkerLogStore(factory), new PentaStore(factory), 
+                                           new TestStore(factory), new TestBranchStore(factory), new AutobenchStateStore(factory));
+        
+        // Insert user with an access token.
+        using var context = factory.CreateDbContext();
+        var accessToken = "123456789";
+        context.Users.Add(new ApplicationUser
+        {
+            Tests = [],
+            OpeningBooks = [],
+            PasswordHash = "123456",
+            UserName = "test",
+            AccessToken = accessToken
+        });
+        context.SaveChanges();
+        _controller.HttpContext.Request.Headers.Add(new (Shared.WORKER_REQUEST_HEADER, accessToken));
+        
+        // Create 
+
+    }
+
+    [Test]
+    public void Test()
+    {
+    }
     
 }
---
+++ WebApplication.Tests/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+
+namespace WebApplication.Tests;
+
+public class TestContextFactory : IDbContextFactory<ApplicationDbContext>
+{
+    /// <inheritdoc /> 
+    public ApplicationDbContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("TEST_DATABASE")
+            .Options;
+
+        var instance = new ApplicationDbContext(options);
+        return instance;
+    }
+}
