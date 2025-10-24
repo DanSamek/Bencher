e24175032bf2e692dd7950f22e67f82f3d3f9702
Daniel Samek <samekda3@fit.cvut.cz>
2025-10-24 21:33:17 +0200

    Better forms for login/register/manage

--- WebApplication/wwwroot/js/site.js
+++ WebApplication/wwwroot/js/site.js
@@ -1,20 +1,21 @@
-
 const THEME_KEY = 'theme';
 const LIGHT_THEME_VALUE = 'light';
-function setTheme(theme){
+
+function setTheme(theme) {
     localStorage.setItem(THEME_KEY, theme);
     ensureTheme(theme);
 }
 
-function getTheme(){
+function getTheme() {
     const theme = localStorage.getItem(THEME_KEY);
     return theme == null ? LIGHT_THEME_VALUE : theme;
 }
 
-function ensureTheme(theme){
+function ensureTheme(theme) {
     document.documentElement.setAttribute('data-bs-theme', theme);
 }
 
-window.onload = function (){
-   ensureTheme(getTheme()); 
-};
+document.addEventListener("DOMContentLoaded", function (event) {
+    ensureTheme(getTheme());
+});
+
