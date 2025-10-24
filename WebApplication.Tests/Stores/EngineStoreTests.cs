5efa93fa98dbf9439ecf5d7f55b42d5fa32b88ae
Daniel Samek <samekda3@fit.cvut.cz>
2025-10-24 15:05:54 +0200

    EngineStore tests

---
+++ WebApplication.Tests/Stores/EngineStoreTests.cs
@@ -0,0 +1,83 @@
+using System.Text;
+using WebApplication.Stores;
+using WebApplication.Tests.Builders;
+
+namespace WebApplication.Tests.Stores;
+
+[TestFixture]   
+public class EngineStoreTests : TestBase
+{
+    /// <summary>
+    /// Test for <see cref="EngineStore.DeleteById"/>
+    /// </summary>
+    [Test]
+    public void DeleteById()
+    {
+        new DomainBuilder(Factory.CreateDbContext())
+            .CreateUser("test_user")
+                .AddEngine("test_engine")
+                .Close()
+            .Close()
+        .Close();
+
+        var store = new EngineStore(Factory);
+        var testId = Factory.CreateDbContext().Engines.First().Id;
+        store.DeleteById(testId);
+        Assert.That(Factory.CreateDbContext().Engines.Count(), Is.EqualTo(0));
+    }
+    
+    /// <summary>
+    /// Test for <see cref="EngineStore.GetEnginesForUser"/>
+    /// </summary>
+    [Test]
+    public void GetEnginesForUser()
+    {
+        new DomainBuilder(Factory.CreateDbContext())
+            .CreateUser("test_user")
+                .AddEngine("test_engine")
+                .Close()
+                .AddEngine("test_engine_2")
+                .Close()
+            .Close()
+            .CreateUser("test_user_2")
+                .AddEngine("test_engine_3")
+                .Close()
+            .Close()
+        .Close();
+
+        var userId =  Factory.CreateDbContext().Users.First().Id; 
+        
+        var store = new EngineStore(Factory);
+        var engines = store.GetEnginesForUser(userId);
+        
+        Assert.That(engines, Is.Not.Null);
+        Assert.That(engines, Has.Count.EqualTo(2));
+        Assert.That(engines[0].Name, Is.EqualTo("test_engine"));
+        Assert.That(engines[1].Name, Is.EqualTo("test_engine_2"));
+    }
+
+    /// <summary>
+    /// Test for <see cref="EngineStore.Add(string, string, string, string)"/>
+    /// </summary>
+    [Test]
+    public void Add()
+    {
+        new DomainBuilder(Factory.CreateDbContext())
+            .CreateUser("test_user")
+            .Close()
+        .Close();
+        
+        var store = new EngineStore(Factory);
+        
+        var userId = Factory.CreateDbContext().Users.First().Id;
+        store.Add("stockfish", "https://github.com/DanSamek/Stockfish", "make -j profile-build", userId);
+        
+        Assert.That(Factory.CreateDbContext().Engines.Count(), Is.EqualTo(1));
+        var test = Factory.CreateDbContext().Engines.First();
+        
+        Assert.That(test.Name, Is.EqualTo("stockfish"));
+        Assert.That(test.GitUrl, Is.EqualTo("https://github.com/DanSamek/Stockfish"));
+        Assert.That(Encoding.ASCII.GetString(test.BuildScript), Is.EqualTo("make -j profile-build"));
+        
+    }
+}
