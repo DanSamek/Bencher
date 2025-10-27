a5d8d469d4da2a1cef0cb57649917e65897e3605
Daniel Samek <samekda3@fit.cvut.cz>
2025-10-27 20:55:42 +0100

    Added monaco editor, loading bar, responsive table for all engines

--- WebApplication/Stores/EngineStore.cs
+++ WebApplication/Stores/EngineStore.cs
@@ -72,7 +72,7 @@ public class EngineStore : Store<Engine>
         var bytes = Engine.GetBuildScriptBytes(buildScript);
         GetDbSet()
             .Where(e => e.Id == engineId)
-            .ExecuteUpdateAsync(spc =>
+            .ExecuteUpdate(spc =>
                 spc.SetProperty(t => t.Name, name)
                     .SetProperty(t => t.GitUrl, gitUrl)
                     .SetProperty(t => t.BuildScript, bytes)
--- WebApplication/wwwroot/js/site.js
+++ WebApplication/wwwroot/js/site.js
@@ -19,3 +19,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
     ensureTheme(getTheme());
 });
 
+function registerMonacoEditor(elementId, language, initialValue) {
+    require.config({ paths: { 'vs': 'lib/monaco-editor/min/vs' }});
+    require(['vs/editor/editor.main'], function() {
+        const theme = getTheme() === LIGHT_THEME_VALUE ? 'vs' : 'vs-dark';
+        var editor = monaco.editor.create(document.getElementById(elementId),{
+            value: initialValue,
+            language: language,
+            automaticLayout: true,
+            theme: theme
+        });
+    });
+}
